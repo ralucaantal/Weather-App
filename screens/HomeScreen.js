2e3ae71697af581e25a03cbc6ab415fb290993c1
Raluca Antal-Vaida <ralucaantal0609@gmail.com>
2024-05-25 14:11:03 +0300

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Buna ziua! Merge?</Text>
+    </View>
+  );
+}
